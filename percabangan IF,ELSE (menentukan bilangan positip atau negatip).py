96468a7a0305484bc25ef5cbff8741c0ac0b7b41
Bryanbray <150464805+Bryanbray@users.noreply.github.com>
2023-11-10 23:44:01 +0700

    Add files via upload

---
+++ percabangan IF,ELSE (menentukan bilangan positip atau negatip).py
@@ -0,0 +1,6 @@
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+if angka1 > angka2:
+    print(angka1, "adalah angka terbesar.")
+else:
+    print(angka2, "adalah angka terbesar.")
